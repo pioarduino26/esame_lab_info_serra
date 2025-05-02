192e243b3ed847a2aa4dd686985928afd5ad33dd
pioarduino26 <antoniooliverio2004@gmail.com>
2025-05-02 09:41:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "ausiliari/sensoristica.h"
+#include <string.h>
 
+void stampaSerre(SerraDati serre[], int n) {
+    printf("\nStato delle serre:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Serra %d: %s - Umidita' terreno: %d\n", i + 1, serre[i].pianta.nome, serre[i].umidita_terreno);
+    }
+    printf("\n");
+}
