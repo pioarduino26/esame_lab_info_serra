406e1ffb475dd094344bf55374b3ebb923dc7cdb
Damiano2563 <tornadodark25@gmail.com>
2025-05-02 10:12:36 +0200

    Aggiunta funzione per stampare lo stato delle serre e salvare i log su file
    Ho implementato le funzioni stampaSerre per visualizzare le informazioni delle serre e salvaLog per il salvataggio dei dati in un file specificato dall'utente.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include "ausiliari/sensoristica.h"
 #include <string.h>
-//stampo le serre in base alla tipologia di piante
+// Funzione per stampare lo stato delle serre in base alla tipologia di pianta
 void stampaSerre(SerraDati serre[], int n) {
     printf("\nStato delle serre:\n");
     for (int i = 0; i < n; i++) {
@@ -12,15 +12,16 @@ void stampaSerre(SerraDati serre[], int n) {
     }
     printf("\n");
 }
-// creo il file per il salvataggio dei dati serra
+// Stampa informazioni sulla serra: indice, nome della pianta e umidità del terreno
 void salvaLog(SerraDati serre[], int n) {
     printf("Salva il log delle attivita' delle serre.\n");
     printf("Inserisci il nome del file in cui vuoi salvare il log: ");
     char nomeFile[100];
     scanf("%99s", nomeFile);
 
-    FILE* fileSalvataggio = fopen(nomeFile, "w");
+    FILE* fileSalvataggio = fopen(nomeFile, "w");// Apertura del file in scrittura
     if (!fileSalvataggio) {
+        // Messaggio di errore se il file non può essere creato/aperto
         printf("Errore: Impossibile creare il file di salvataggio.\n");
         return;
     }
