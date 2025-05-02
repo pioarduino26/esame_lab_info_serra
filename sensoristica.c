de8dd6d73a3fa0007f6249b4c7e307d34fe75d57
pioarduino26 <antoniooliverio2004@gmail.com>
2025-05-02 10:07:14 +0200

    Update sensoristica.c

--- sensoristica.c
+++ sensoristica.c
@@ -1,1 +1,9 @@
-
+// in questo file sono contenute le varie funzioni del programma
+#include "sensoristica.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#define MIN_VALUE 724
+#define MAX_VALUE 1000
