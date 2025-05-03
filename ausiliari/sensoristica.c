2d2b2ba468c807e84787a400ea9425cd46fac1b0
Damiano2563 <tornadodark25@gmail.com>
2025-05-03 09:18:33 +0200

    Update sensoristica.c: implementato algoritmo di ordinamento per ordinare le serre in base all'umidità del terreno.
    Aggiunta funzione selectionSort per ordinare l'array di serre in ordine crescente di umidità del terreno

---
+++ ausiliari/sensoristica.c
@@ -0,0 +1,23 @@
+// in questo file sono contenute le varie funzioni del programma
+#include "sensoristica.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#define MIN_VALUE 724
+#define MAX_VALUE 1000
+void selectionSort(SerraDati serre[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (serre[j].umidita_terreno < serre[min_idx].umidita_terreno) {
+                min_idx = j;
+            }
+        }
+        // Scambio
+        SerraDati temp = serre[i];
+        serre[i] = serre[min_idx];
+        serre[min_idx] = temp;
+    }
+}
