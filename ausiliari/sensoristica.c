ac5f4db12352ec19424eefaa0aa2540f7d1f380a
pioarduino26 <antoniooliverio2004@gmail.com>
2025-05-07 20:04:16 +0200

    modifica a sensoristica.c
    ho aggiunto la funzione per l'applicazione dell'insetticida nella serra

--- ausiliari/sensoristica.c
+++ ausiliari/sensoristica.c
@@ -21,3 +21,53 @@ void selectionSort(SerraDati serre[], int n) {
         serre[min_idx] = temp;
     }
 }
+void irrigazioneInsetticida(SerraDati serre[], int *n) {
+    char risposta[4];
+
+    printf("\nAttenzione: alcune piante potrebbero essere sensibili all'insetticida.\n");
+    printf("Vuoi applicarlo a tutte le serre? (si/no): ");
+    scanf("%3s", risposta);
+
+    if (strcmp(risposta, "si") == 0) {
+        printf("\nAvvio del trattamento con insetticida su tutte le serre...\n");
+        sleep(1);
+        for (int i = 0; i < *n; i++) {
+                 if (serre[i].pianta.resistenza_insetticida == 0) {
+                    strappaPiantaESostituisci(&serre[i]);
+                 }
+
+             else {
+                printf("La pianta '%s' e' resistente. Nessuna sostituzione effettuata.\n", serre[i].pianta.nome);
+            }
+            printf("Trattamento insetticida applicato alla serra %d (%s).\n", i + 1, serre[i].pianta.nome);
+        }
+        printf("Trattamento completato.\n");
+
+    } else {
+        printf("\nHai deciso di applicare l'insetticida solo su una serra.\n");
+        printf("Premi INVIO per visualizzare l'elenco delle serre e scegliere quella desiderata...\n");
+
+        while (getchar() != '\n');
+        getchar();
+
+        printf("\nElenco delle serre:\n");
+        for (int i = 0; i < *n; i++) {
+            printf("  %d) %s\n", i + 1, serre[i].pianta.nome);
+        }
+        printf("\nInserisci il numero della serra scelta: ");
+        int scelta;
+        scanf("%d", &scelta);
+
+        if (scelta >= 1 && scelta <= *n) {
+            if (serre[scelta - 1].pianta.resistenza_insetticida == 0) {
+                strappaPiantaESostituisci(&serre[scelta - 1]);
+            } else {
+                printf("La pianta '%s' ha resistito. Nessuna sostituzione necessaria.\n", serre[scelta - 1].pianta.nome);
+            }
+            printf("Trattamento insetticida applicato alla serra %d (%s).\n", scelta, serre[scelta - 1].pianta.nome);
+            printf("Trattamento completato.\n");
+        } else {
+            printf("Scelta non valida. Nessun trattamento eseguito.\n");
+        }
+    }
+}
