142c852ad7c892c269b13a14b86e6faf8d1ce0b0
Damiano2563 <tornadodark25@gmail.com>
2025-05-10 11:10:56 +0200

    Aggiornato sensoristica.h con dichiarazione funzione determinaStagione
    Ho aggiunto la dichiarazione della funzione determinaStagione in sensoristica.h.

--- ausiliari/sensoristica.h
+++ ausiliari/sensoristica.h
@@ -6,7 +6,7 @@
 // Definizione della struttura Pianta
 typedef struct {
     char nome[20]; // dichiaro un array di caratteri di 20 elementi
-    int umidita_min; 
+    int umidita_min;
     int umidita_max;
     int resistenza_insetticida; // 1 = Resistente, 0 = Debole
 } Pianta;
@@ -26,3 +26,4 @@ void selectionSort(SerraDati serre[], int n);
 void irrigazioneInsetticida(SerraDati serre[], int *n);
 void strappaPiantaESostituisci(SerraDati *serra);
 void salvaStoricoRec(SerraDati serre[], int n, int index, FILE* fp);
+const char* determinaStagione(int mese); // Restituisce la stagione in base al mese
