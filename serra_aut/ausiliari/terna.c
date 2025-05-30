a5d38d26ad0a7cdbe931a2f558f36ec43401aa7e
pioarduino26 <antoniooliverio2004@gmail.com>
2025-05-30 15:54:04 +0200

    creazione terna.c

---
+++ serra_aut/ausiliari/terna.c
@@ -0,0 +1,28 @@
+#include "terna.h"
+#include "sensoristica.h"  // Includi il file che contiene la funzione di controllo
+
+// Definizione dei valori fissi per le serre (temperatura e umidità del terreno)
+ValoriFissi valori_fissi[] = {
+    {21, 1000},   // Basilico
+    {25, 950},    // Peperoncino
+    {17, 900},    // Cipolla
+    {20, 920},    // Margherita
+    {20, 940},    // Tulipano
+    {22, 930}     // Dente di Leone
+}; //uso dei valori fissi più conveniente e meno laborioso rispetto all'uso di rand()
+
+
+
+// Funzione di controllo della temperatura
+void controlloTemperatura(SerraDati serre[], int n) {
+        // Confronta la temperatura letta con quella fissa
+        if (serre[n].temperatura < (valori_fissi[n].temperatura_fissa - 5) ||
+            serre[n].temperatura > (valori_fissi[n].temperatura_fissa + 5)) {
+            printf("La temperatura della serra %d e'fuori range! Correggo il valore a...\n", n + 1);
+            // Correggi la temperatura
+            serre[n].temperatura = valori_fissi[n].temperatura_fissa;
+            printf("La temperatura della serra %d e' stata ripristinata a: %d C\n", n + 1, serre[n].temperatura);
+        } else {
+            printf("La temperatura della serra %d e' corretta ed e': %d C\n", n + 1, serre[n].temperatura);
+        }
+}
