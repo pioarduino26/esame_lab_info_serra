a0a805ea41ae42b0e846d2a9a847ea0c526ab76f
Damiano2563 <tornadodark25@gmail.com>
2025-06-05 18:28:41 +0200

    Aggiornamento  effettuato a valori fissi e all'ordinamento
    Ho aggiornato i valori fissi per la temperatura e l'umidità del terreno nelle serre e ho corretto  il passaggio dell'indice nelle chiamate a leggiSensori(). Ho quindi modificato le
    funzioni di ordinamento per utilizzare valori fissi.

--- serra_aut/ausiliari/sort.c
+++ serra_aut/ausiliari/sort.c
@@ -4,6 +4,10 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
+#include "sensoristica.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "terna.h"
 #define MIN_VALUE 724
 #define MAX_VALUE 1000
 
@@ -13,7 +17,7 @@ void selectionSort(SerraDati serre[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
         for (int j = i + 1; j < n; j++) {
-            if (serre[j].umidita_terreno < serre[min_idx].umidita_terreno) {
+            if(valori_fissi[j].umidita_terreno_fissa < valori_fissi[min_idx].umidita_terreno_fissa) {
                 min_idx = j;
             }
         }
@@ -27,7 +31,7 @@ void selectiontemp(SerraDati serre[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
         for (int j = i + 1; j < n; j++) {
-            if (serre[j].temperatura < serre[min_idx].temperatura) {
+            if (valori_fissi[j].temperatura_fissa < valori_fissi[min_idx].temperatura_fissa) {
                 min_idx = j;
             }
         }
